da81cbb6fff1630b2d5e1777423259dccf755a17
Faizan Ahmad <76480695+faizan-19@users.noreply.github.com>
2025-06-16 15:28:44 +0500

    version update

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<uni_links5/UniLinksPlugin.h>)
+#import <uni_links5/UniLinksPlugin.h>
+#else
+@import uni_links5;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [UniLinksPlugin registerWithRegistrar:[registry registrarForPlugin:@"UniLinksPlugin"]];
+}
+
+@end
